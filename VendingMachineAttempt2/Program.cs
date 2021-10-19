5fe6f55133d30841614e199b8a204cda67d85ea7
George Sydby <george.sydby@gmail.com>
2021-10-19 16:19:19 +0200

    Med hjälp av Tobias, lagd grunden för VendingMachine interface

--- VendingMachineAttempt2/Program.cs
+++ VendingMachineAttempt2/Program.cs
@@ -7,6 +7,41 @@ namespace VendingMachineAttempt2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    // Interface
+    interface IVending
+    {
+        void Purchase(); // interface method (does not have a body)
+        void ShowAll();
+        void InsertMoney();
+        void EndTransaction();
+    }
+
+    // VendingMachine "implements" the IVending interface
+    class VendingMachine : IVending
+    {
+        public void EndTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertMoney()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Purchase()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+
+        public void ShowAll()
+        {
+            throw new NotImplementedException();
         }
     }
 }
