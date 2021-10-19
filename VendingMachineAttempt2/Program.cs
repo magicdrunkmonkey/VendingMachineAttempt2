0327accf2314cb9fc3f9e9b4871ca5d5561d0706
George Sydby <george.sydby@gmail.com>
2021-10-19 19:06:48 +0200

    Commented on what the methods in the class VendingMachine does.

--- VendingMachineAttempt2/Program.cs
+++ VendingMachineAttempt2/Program.cs
@@ -8,7 +8,7 @@ namespace VendingMachineAttempt2
         {
             //Fields, declare, initialize
             int moneyPool;
-            readonly int[] moneyValue = { 1, 5, 10, 20, 50, 100, 1000 };   //Why does this not work?
+            //readonly int[] moneyValue = { 1, 5, 10, 20, 50, 100, 1000 };   //Why does this not work?
                     
             //Console.WriteLine("Hello World!");
         }
@@ -26,22 +26,27 @@ namespace VendingMachineAttempt2
     // VendingMachine "implements" the IVending interface
     class VendingMachine : IVending
     {
+        //To return unused money left in the moneypool
         public void EndTransaction()
         {
             throw new NotImplementedException();
         }
 
+        //To add money to the moneypool
         public void InsertMoney()
         {
+            //readonly int[] moneyValue = { 1, 5, 10, 20, 50, 100, 1000 };
             throw new NotImplementedException();
         }
 
+        //To purchase a product from the vendingmachine
         public void Purchase()
         {
             // The body of animalSound() is provided here
             Console.WriteLine("The pig says: wee wee");
         }
 
+        //Show all available products with prices in the vendingmachine
         public void ShowAll()
         {
             throw new NotImplementedException();
