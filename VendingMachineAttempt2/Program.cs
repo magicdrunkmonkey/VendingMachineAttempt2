9ad02ded5fc020409c05aea12c0dab07be60d4e8
George Sydby <george.sydby@gmail.com>
2021-10-19 19:09:15 +0200

    Update comments

--- VendingMachineAttempt2/Program.cs
+++ VendingMachineAttempt2/Program.cs
@@ -32,21 +32,22 @@ namespace VendingMachineAttempt2
             throw new NotImplementedException();
         }
 
-        //To add money to the moneypool
+        //To add money to the moneypool.
         public void InsertMoney()
-        {
-            //readonly int[] moneyValue = { 1, 5, 10, 20, 50, 100, 1000 };
+        {            
             throw new NotImplementedException();
         }
 
         //To purchase a product from the vendingmachine
+        //with money in the moneypool.
         public void Purchase()
         {
             // The body of animalSound() is provided here
             Console.WriteLine("The pig says: wee wee");
         }
 
-        //Show all available products with prices in the vendingmachine
+        //Show all available products with prices
+        //the vendingmachine has.
         public void ShowAll()
         {
             throw new NotImplementedException();
